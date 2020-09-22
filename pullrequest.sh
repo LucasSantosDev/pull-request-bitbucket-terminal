debff1d994d4ac4b83fb55941860045d44c33341
Lucas da Silva Santos <lucas_ss.arts@live.com>
2020-09-22 14:07:40 -0300

    Update pullrequest.sh

--- pullrequest.sh
+++ pullrequest.sh
@@ -8,17 +8,29 @@
 #########################################
 
 # Command to RUN create PR
-# sh pullrequest.sh development ´title´ ´descriprion´ (pode passar vazio também) -- modificar os ´ por aspas
+# sh pullrequest.sh
+
+read -p "Enter title PR (default): " title
+read -p "Enter description (optional): " description
+
+current_branch="$(git branch --show-current)"
+
+if [ "$title" = 'default' ];
+then
+        title=$current_branch
+fi
 
 cat pullrequest.json | \
-	jq '.source.branch.name = $new_branch' --arg new_branch $(git branch --show-current) | \
-	jq '.destination.branch.name = $destination' --arg destination "$1" | \
-	jq '.title = $title' --arg title "$2" | \
-	jq '.description = $description' --arg description "$3" | \
-	jq '.source.repository.full_name = $repository' --arg repository "$(git config --get remote.origin.url | sed 's/.*\/\([^ ]*\/[^.]*\).*/\1/')" > pullrequest_copy.json
+        jq '.source.branch.name = $new_branch' --arg new_branch "$current_branch" | \
+        jq '.destination.branch.name = $destination' --arg destination "$(git log --pretty=format:'%D' HEAD^ | grep 'origin/' | head -n1 | sed 's@origin/@@' | sed 's@,.*@@'
+)" | \
+        jq '.title = $title' --arg title "$title" | \
+        jq '.description = $description' --arg description "$description" | \
+        jq '.reviewers[0].username = $reviewer' --arg reviewer "nickname_reviewer" | \
+        jq '.source.repository.full_name = $repository' --arg repository "$(git config --get remote.origin.url | sed 's/.*\/\([^ ]*\/[^.]*\).*/\1/')" > pullrequest_copy.json
 
 rm pullrequest.json
 
 mv pullrequest_copy.json pullrequest.json
 
-curl -X POST -H "Content-Type: application/json" -u $(git config user.email) https://bitbucket.org/api/2.0/repositories/$(git config --get remote.origin.url | sed 's/.*\/\([^ ]*\/[^.]*\).*/\1/')/pullrequests -d @pullrequest.json
+curl -X POST -H "Content-Type: application/json" -u $(git config user.email) https://bitbucket.org/api/2.0/repositories/$(git config --get remote.origin.url | sed 's/.*\/\([^ ]*\/[^>
